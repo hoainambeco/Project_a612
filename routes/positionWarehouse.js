85b41e86428cfcdb7d6ff7877a9d7ead29965137
hoainambeco <namxg1@gmail.com>
2022-06-10 10:45:19 +0700

    done project a612

--- routes/positionWarehouse.js
+++ routes/positionWarehouse.js
@@ -7,4 +7,5 @@ router.get('/:idWarehouse', positionController.getPositionWarehouse);
 router.post('/add', positionController.postPositionWarehouses);
 router.post('/edit/:id', positionController.editPositionWarehouses);
 router.post('/delete/:idWarehouse', positionController.deletePositionWarehouses);
+router.get('/delete/:idWarehouse', positionController.deletePositionWarehouses);
 module.exports = router;
