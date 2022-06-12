c13b3bc0f416cba78a48262cd0bbbd502f538762
hoainambeco <namxg1@gmail.com>
2022-06-12 12:10:59 +0700

    tăng tốc server

--- routes/positionWarehouse.js
+++ routes/positionWarehouse.js
@@ -5,6 +5,7 @@ const positionController = require('../controllers/positionWarehouses.controller
 router.get('/', positionController.getPositionWarehouses);
 router.get('/:idWarehouse', positionController.getPositionWarehouse);
 router.post('/add/:idWarehouse', positionController.postPositionWarehouses);
+router.get('/add/:idWarehouse', positionController.postPositionWarehouses);
 router.post('/edit/:id', positionController.editPositionWarehouses);
 router.post('/delete/:idWarehouse', positionController.deletePositionWarehouses);
 router.get('/delete/:idWarehouse', positionController.deletePositionWarehouses);
