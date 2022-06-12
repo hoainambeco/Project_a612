b44200e6cb9ce080bf9e4faeb03217a46b8a97d2
hoainambeco <namxg1@gmail.com>
2022-06-12 14:25:46 +0700

    sửa lại api positionWarehouses.controller.js

--- controllers/positionWarehouses.controller.js
+++ controllers/positionWarehouses.controller.js
@@ -2,11 +2,11 @@ const positionWarehousesModel = require('../models/positions.model');
 const khoHangModel = require('../models/khoHang.models');
 
 exports.getPositionWarehouses = async (req, res) => {
-    const positionWarehousesList = await positionWarehousesModel.find({});
+    const positionWarehousesList = await positionWarehousesModel.find({}).sort('namePosition');
     res.json(positionWarehousesList);
 }
 exports.getPositionWarehouse = async (req, res) => {
-    const listPositions = await positionWarehousesModel.find({idWarehouse: req.params.idWarehouse});
+    const listPositions = await positionWarehousesModel.find({idWarehouse: req.params.idWarehouse}).sort('namePosition');
     res.json(listPositions);
 }
 exports.postPositionWarehouses = async (req, res) => {
@@ -16,9 +16,9 @@ exports.postPositionWarehouses = async (req, res) => {
         if (listPositions.length > 0) {
             return res.json({'status': 'Kho hàng đã có vị trí'});
         }
-        for (let i = 0; i < warehouse.floors; i++) {
-            for (let j = 0; j < warehouse.row; j++) {
-                for (let k = 0; k < warehouse.position; k++) {
+        for (let i = 1; i <= warehouse.floors; i++) {
+            for (let j = 1; j <= warehouse.row; j++) {
+                for (let k = 1; k <= warehouse.position; k++) {
                     const objpositionWarehouses = new positionWarehousesModel({
                         idWarehouse: req.params.idWarehouse,
                         namePosition: `F${i}-R${j}-C${k}`,
@@ -41,6 +41,10 @@ exports.postPositionWarehouses = async (req, res) => {
 
 exports.editPositionWarehouses = async (req, res) => {
     const position = await positionWarehousesModel.findOne({_id: req.params.id});
+    console.log(position);
+    if(position == null){
+        return res.json({'status': 'Không tìm thấy vị trí'});
+    }
     const objpositionWarehouses = {
         idWarehouse: req.body.idWarehouse || position.idWarehouse,
         namePosition: req.body.namePosition || position.namePosition,
